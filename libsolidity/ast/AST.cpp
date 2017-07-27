06348215ffc2fd5bc900c8e445432f099f6596a1
Alex Beregszaszi <alex@rtfs.hu>
2017-07-27 20:56:29 +0100

    Disallow externalSignature for fallback functions

--- libsolidity/ast/AST.cpp
+++ libsolidity/ast/AST.cpp
@@ -368,6 +368,7 @@ TypePointer FunctionDefinition::type() const
 
 string FunctionDefinition::externalSignature() const
 {
+	solAssert(!isFallback(), "Fallback function has no signature.");
 	return FunctionType(*this).externalSignature();
 }
 
